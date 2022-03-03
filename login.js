c97891d3cea4c81ccc1dd740cd6c685b99350f1d
Juleshb <habaruremajules@gmail.com>
2022-03-03 22:09:48 +0200

    LoginLogout

--- login.js
+++ login.js
@@ -1,33 +1,60 @@
-//import { initializeApp } from "https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"
-//import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js";
- 
- // For Firebase JS SDK v7.20.0 and later, measurementId is optional
- const firebaseConfig = {
-    apiKey: "AIzaSyAfIRAQZe7a8wfuxzQBPOhp85A-vSP_uh8",
-    authDomain: "blog-d990f.firebaseapp.com",
-    projectId: "blog-d990f",
-    storageBucket: "blog-d990f.appspot.com",
-    messagingSenderId: "554054001432",
-    appId: "1:554054001432:web:396b76593255b39456b46d",
-    measurementId: "G-C7QRHX21MK"
-  };
-  firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-db.settings({ timestampInSnapshots: true });
-const auth = getAuth(app);
+const loginForm = document.getElementById('loginform');
+loginForm.addEventListener('click', (e) => {
+ e.preventDefault();
+ // get user info
+ var email =  document.getElementById('email').value;
+ var password= document.getElementById('password').value;
+ /*if (validate_email(email) == false || validate_password(password) == false) {
+  alert('Email or Password is Outta Line!!')
+  return
+  }
+  */
+ // log the user in
+ auth.signInWithEmailAndPassword(email, password).then((cred) => {
+   console.log(auth);
+   //loginForm.reset();
+   window.location.assign('Dashboard/dash.html');
+}).catch(error => {
+ alert(error.message);
+})
+});
 
-document.getElementById("save").addEventListener('click', function () {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("pass").value;
+/*const logInForm = document.querySelector('#loginform'); // creating a variable to access the log in form from login.html
 
-    signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    const user = userCredential.user;
- 
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
+// login authentication
+/*logInForm.addEventListener('click', (e)=>{
+    e.preventDefault()
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    firebase.auth().signInWithEmailAndPassword(email,password)
+    .then((userCredential) => {
+    console.log("Logged In")
+    location.href='dashboard.html'
   });
 
+})
+
+/*
+window.onload=function(){
+
+const logInForm = document.querySelector('btnn');
+logInForm.addEventListener('click', (e) => {
+ e.preventDefault();
+ // get user info
+ var email =  document.getElementById('email').value;
+ var password= document.getElementById('password').value;
+ if (validate_email(email) == false || validate_password(password) == false) {
+  alert('Email or Password is Outta Line!!')
+  return
+  }
+ // log the user in
+ auth.signInWithEmailAndPassword(email, password).then((cred) => {
+   console.log(auth);
+   logInForm.reset();
+   window.location.replace('/Dashboard/dash.htm');
+}).catch(error => {
+ alert(error.message);
+})
 });
+}
+*/
--- main.js
+++ main.js
@@ -1,14 +1,63 @@
 
-const blog = document.querySelector('#blogs');
+const renderPosts = (doc) => {
+  const postsContainer = document.querySelector('#pr');
+
+  const postDiv = document.createElement('div');
+  postDiv.setAttribute('class', 'post');
+  postDiv.className = 'project';
+
+      const postImage = document.createElement('img');
+      postImage.src = doc.data().image;
+      postImage.className = 'kiki';
+
+      const postData = document.createElement('span');
+      //postData.className = 'project';
+
+          const postTitle = document.createElement('span');
+          postTitle.className = 'title';
+          postTitle.textContent = doc.data().title;
+
+          const postSummary = document.createElement('span');
+          postSummary.textContent = doc.data().content.substring(0, 100);
+          
+          const readMore = document.createElement('button');
+          readMore.textContent = "Read More";
+
+          readMore.addEventListener('click', (e) => {
+              e.preventDefault();
+              location.href = "more.html#"+doc.id;
+          })
+
+         
+      postData.appendChild(postTitle);
+      postData.appendChild(postSummary); 
+      postData.appendChild(readMore);
+
+  postDiv.appendChild(postImage);
+  postDiv.appendChild(postData);
+
+  postsContainer.appendChild(postDiv);
+}
+
+db.collection('blog').get().then(res=>{
+  res.docs.forEach(doc=>{
+      renderPosts(doc);
+  })
+})
+/*
+// my own codes
+
+const blog = document.querySelector('#pr');
 const form = document.querySelector('#post');
 
 // CREATE ELEMENT
 
 function renderBlog(doc) {
-    let td = document.createElement('tr');
-    let date = document.createElement('td');
-    let content = document.createElement('td');
-    let title = document.createElement('td');
+    let td = document.createElement('p');
+    let date = document.createElement('span');
+    let content = document.createElement('span');
+    let title = document.createElement('span');
+   
 
     
 
@@ -16,12 +65,15 @@ function renderBlog(doc) {
     date.textContent = doc.data().date;
     title.textContent = doc.data().title;
     content.textContent = doc.data().content;
+
     
 
 
     td.appendChild(date);
     td.appendChild(title);
     td.appendChild(content);
+    
+  
    
 
 
@@ -39,6 +91,7 @@ db.collection('blog').get().then((snapshot) => {
     })
   })
 
+
   //saving data
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -50,5 +103,23 @@ form.addEventListener('submit', (e) => {
     form.date.value = '';
     form.title.value = '';
     form.content.value = '';
+  });
+
+  const ref = firebase.storage().ref();
+const file = document.querySelector('#photo').files[0]
+const name = (+new Date()) + '-' + file.name;
+const metadata = {
+  contentType: file.type
+};
+const task = ref.child(name).put(file, metadata);
+task
+  .then(snapshot => snapshot.ref.getDownloadURL())
+  .then((url) => {
+    console.log(url);
+    document.querySelector('#someImageTagID').src = url;
   })
-  
+  .catch(console.error);
+
+  //update
+
+  */
